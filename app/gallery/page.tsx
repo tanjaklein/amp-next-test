10001c41823ce0c27fd979de0e3238054757420e
Tanja Valkin <tanjavalkin@gmail.com>
2025-07-12 16:21:39 +0200

    Added Amplify.config

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,6 +1,11 @@
 "use client"
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import { Amplify } from "aws-amplify";
+
+import outputs from "../amplify_outputs.json"
+
+Amplify.configure(outputs)
 
 const client = generateClient<Schema>() ;// use this Data client for CRUDL requests
 
