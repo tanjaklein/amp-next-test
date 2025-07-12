c4cddf334e7d7f3d0db4f0b10f1020b9679de56c
Tanja Valkin <tanjavalkin@gmail.com>
2025-07-12 18:02:27 +0200

    package.json

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -3,7 +3,7 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from "aws-amplify";
 
-import outputs from "../amplify_outputs.json"
+import outputs from "@/amplify_outputs.json"; // Import the Amplify outputs file
 
 Amplify.configure(outputs)
 
