69924a7e972f41474eb1b0868945b87f8008520d
Tanja Valkin <tanjavalkin@gmail.com>
2025-07-15 13:50:52 +0200

    with images

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -6,6 +6,9 @@ import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from "aws-amplify";
 import { useState, useEffect}  from "react";
 
+// Update the import path to match the actual file location and name
+import   ArtsGrid   from '../../components/artimage/art-grid';
+
 
 import outputs from "@/amplify_outputs.json"; // Import the Amplify outputs file
 
@@ -23,11 +26,11 @@ const client = generateClient<Schema>() ;// use this Data client for CRUDL reque
 
 
 export  function Gallery () {
-   const [todos, setTodos] = useState<Array<Schema["Artwork"]["type"]>>([]);
+   const [arts, setArts] = useState<Array<Schema["Artwork"]["type"]>>([]);
 
    useEffect(() => {
     client.models.Artwork.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+      next: (data) => setArts([...data.items]),
     });
   }, []);
    
@@ -38,12 +41,8 @@ export  function Gallery () {
    </header>
    <div>
      <p>Welcome to the Gallery page!</p>
-     <ul>
-        {todos.map((todo) => (
-          <li 
-                  key={todo.id}>{todo.name}</li>
-        ))}
-      </ul>
+     <ArtsGrid arts={arts} />
+     
     
      </div>
     
